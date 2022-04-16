f7b0e543827f7935ba85ec1d4c729077d889e0f9
gelfik <bulatgilfanov@gmail.com>
2022-04-16 23:09:55 +0300

    update

--- TatarApp/serializers.py
+++ TatarApp/serializers.py
@@ -4,14 +4,33 @@ from TatarApp.models import TatarMultipleAnswerModel, TatarAskModel
 
 
 class TatarMultipleAnswerSerializer(serializers.ModelSerializer):
+    answerPhoto = serializers.SerializerMethodField(method_name='get_answerPhoto')
+
     class Meta:
         model = TatarMultipleAnswerModel
         fields = ('answer', 'answerPhoto', 'validStatus', 'is_text', 'is_photo')
 
+    def get_answerPhoto(self, instance):
+        if instance.answerPhoto is not None:
+            return 'https://izzibrain.gelfik.dev' + instance.answerPhoto
+        return instance.answerPhoto
+
 
 class TatarAskSerializer(serializers.ModelSerializer):
     answer_list = TatarMultipleAnswerSerializer(many=True)
+    photo = serializers.SerializerMethodField(method_name='get_photo')
+    audio = serializers.SerializerMethodField(method_name='get_audio')
 
     class Meta:
         model = TatarAskModel
         fields = ('ask_variant', 'ask', 'audio', 'photo', 'is_selected', 'is_multiple', 'answer', 'answer_list')
+
+    def get_photo(self, instance):
+        if instance.photo is not None:
+            return 'https://izzibrain.gelfik.dev' + instance.photo
+        return instance.photo
+
+    def get_audio(self, instance):
+        if instance.photo is not None:
+            return 'https://izzibrain.gelfik.dev' + instance.audio
+        return instance.audio
